6cf9c2bf2cb79095776212f0d8df4e3c325c6285
Reuben Berghan <reuben.berghan@gmail.com>
2019-01-21 10:44:19 +1300

    Add Webpack and .npmignore for npm publishing

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,16 @@
-import * as React from 'react'
+import styled from 'styled-components'
 
-export default function Button ({ children }) {
-  return (
-    <button>{children}</button>
-  )
-}
+const Button = styled.button`
+  align-items: center;
+  border-radius: 60px;
+  border: 1px solid blue;
+  color: blue;
+  display: flex;
+  flex-direction: row;
+  height: 40px;
+  justify-content: center;
+  width: 160px;
+`
+Button.displayName = 'Button'
+
+export default Button
